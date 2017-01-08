8e284c014bf30619bc121063b38bad9380f10da1
Wen-Chun Lin <lovecankill@gmail.com>
2017-01-08 13:04:31 +0800

    Initial commit

---
+++ MyHomework1/MyHomework1/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 組件的一般資訊由下列屬性集 
+// 控制。變更這些屬性值可修改組件所關聯的
+// 資訊。
+[assembly: AssemblyTitle("MyHomework1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyHomework1")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform","UAP")]
+
+// 組件的版本資訊由下列四值組成: 
+//
+//      主要版本
+//      次要版本 
+//      組建編號
+//      修訂編號
+//
+// 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建及修訂編號 
+// 指定為預設值: 
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ MyHomework1/MyHomework1/UnitTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyHomework1
+{
+    class SumByGroup
+    {
+        public static IEnumerable<int> sum<T>(IEnumerable<T> xs, int groupSize, Func<T, int> func)
+        {
+            //return new int[] { 6, 15, 24, 21 };
+            return new int[] { 50, 66, 60 };
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SumTest_Costs_GroupSize_3()
+        {
+            //arrange
+            var costs = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+            var expected = new int[] { 6, 15, 24, 21 };
+            
+            //act
+            var actual = SumByGroup.sum<int>(costs, 3, o => o );
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual.ToList());
+        }
+
+        [TestMethod]
+        public void SumTest_Revenue_GroupSize_4()
+        {
+            //arrange
+            var revenue = new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            var expected = new int[] { 50, 66, 60 };
+
+            //act
+            var actual = SumByGroup.sum<int>(revenue, 4, o => o );
+
+            //assert
+            CollectionAssert.AreEqual(expected, actual.ToList());
+        }
+    }
+}
